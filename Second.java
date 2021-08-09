80fb9d736e793e7f343170ab3b83af352d2e1d22
Rajat Shah <rajat.shah@mavenir.com>
2021-08-09 13:54:03 +0530

    changing Second.java and adding function calling in master

--- Second.java
+++ Second.java
@@ -2,5 +2,10 @@ public  class Second{
 
     public static void main(String[] args) {
         System.out.println("Now we are in Modifying the class Java Class adding some line in same file ");
+        new_func_master();
+    }
+    public  static  void new_func_master(){
+
+
     }
 }
